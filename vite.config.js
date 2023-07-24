d27f8b3030ffac91932d890c98acfd40952de65f
Axel Tkacheff <aptkachev@rolftech.ru>
2023-07-24 21:31:56 +0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vite'
+
+const repo = 'type-script-arcanoid-2023'
+
+export default defineConfig(({command, mode, ssrBuild}) => {
+  if (mode == "production") {
+    return {
+      base: `/${repo}/`,
+    }
+  }
+})
